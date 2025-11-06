7fb90106192986852205617a4d17f18885b6fa34
SujithBojjawar <150164746+SujithBojjawar@users.noreply.github.com>
2025-11-06 20:56:03 +0530

    Updated API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1", 
+  baseURL: "https://mentee-book-g0pcbdvnm-sujith-bs-projects.vercel.app/login", 
 });
 
 API.interceptors.request.use((req) => {
